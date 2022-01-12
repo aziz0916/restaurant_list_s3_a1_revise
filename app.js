1e9b1839a79aef60a67d4fdf432bd3d2d93ac009
aziz0916 <aziz0916@yahoo.com.tw>
2022-01-12 14:56:21 +0800

    feat: add mongoose and restaurant seeder

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+const restaurantList = require('./restaurant.json')
+
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurant-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
