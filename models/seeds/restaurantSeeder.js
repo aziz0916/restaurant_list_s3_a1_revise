d35ce9350d1e3fd1e07080180399868fa4c540d5
aziz0916 <aziz0916@yahoo.com.tw>
2022-01-14 23:35:38 +0800

    feat: modify seeder create function

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,13 @@
 const db = require('../../config/mongoose.js')
 const Restaurant = require('../restaurant.js')
-const restaurantList = require('../../restaurant.json')
+const restaurantList = require('../../restaurant.json').results
 
 db.once('open', () => {
-  for (let i = 0; i < restaurantList.results.length; i++) {
-    Restaurant.create({
-      id: restaurantList.results[i].id,
-      name: restaurantList.results[i].name,
-      name_en: restaurantList.results[i].name_en,
-      category: restaurantList.results[i].category,
-      image: restaurantList.results[i].image,
-      location: restaurantList.results[i].location,
-      phone: restaurantList.results[i].phone,
-      google_map: restaurantList.results[i].google_map,
-      rating: restaurantList.results[i].rating,
-      description: restaurantList.results[i].description
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log('done.')
+      db.close() // 關閉MongoDB
     })
-  }
-
-  console.log('done.')
+    .catch(error => console.log(error))
+    .finally(() => process.exit()) // 結束程式執行
 })
