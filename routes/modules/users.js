1694ad2450553649cf458aa69016cc57c7477159
aziz0916 <aziz0916@yahoo.com.tw>
2022-03-30 14:54:37 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 const User = require('../../models/user')
@@ -41,22 +42,24 @@ router.post('/register', (req, res) => {
     .then(user => {
       if (user) {
         errors.push({ message: '這個 Email 已經註冊過了。' })
-        res.render('register', {
+        return res.render('register', {
           errors,
           name,
           email,
           password,
           confirmPassword
         })
-      } else {
-        return User.create({
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
           name,
           email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
-      }
+          password: hash
+        }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 })
