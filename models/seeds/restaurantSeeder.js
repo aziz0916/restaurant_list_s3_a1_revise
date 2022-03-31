885af9c214c758ffada8d5d7661ee4f39fc424f0
aziz0916 <aziz0916@yahoo.com.tw>
2022-03-31 12:04:18 +0800

    modify restaurantSeeder from Promise.all to Async/Await

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -20,28 +20,59 @@ const SEED_USERS = [{
   password: '12345678',
   restaurants: restaurantList.slice(3, 6)
 }]
+// Promise.all方法
+// db.once('open', () => {
+//   Promise.all(Array.from(SEED_USERS, seedUser => {
+//     bcrypt
+//       .genSalt(10)
+//       .then(salt => bcrypt.hash(seedUser.password, salt))
+//       .then(hash => User.create({
+//         name: seedUser.name,
+//         email: seedUser.email,
+//         password: hash,
+//       }))
+//       .then(user => {
+//         const userId = user._id
+//         seedUser.restaurants.map(restaurant => {
+//           restaurant.userId = userId
+//         })
+//         return Restaurant.create(seedUser.restaurants)
+//       })
+//       .then(() => {
+//         console.log('done')
+//         process.exit()
+//       })
+//   }))
+// })
 
-db.once('open', () => {
-  Promise.all(Array.from(SEED_USERS, seedUser => {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(seedUser.password, salt))
-      .then(hash => User.create({
-        name: seedUser.name,
-        email: seedUser.email,
-        password: hash,
-      }))
-      .then(user => {
-        const userId = user._id
-        seedUser.restaurants.map(restaurant => {
-          restaurant.userId = userId
-        })
-        return Restaurant.create(seedUser.restaurants)
-      })
-      .then(() => {
-        console.log('done')
-        process.exit()
-      })
-  }))
+//Async/Await方法
+async function createSeed(seedUser) {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(seedUser.password, salt)
+    const user = await User.create({
+      name: seedUser.name,
+      email: seedUser.email,
+      password: hash
+    })
+    const userId = user._id
+    seedUser.restaurants.forEach(restaurant => {
+      restaurant.userId = userId
+    })
+    await Restaurant.create(seedUser.restaurants)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+db.once('open', async () => {
+  try {
+    for (let i = 0; i < SEED_USERS.length; i++) {
+      await createSeed(SEED_USERS[i])
+    }
+    console.log('done')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+  }
 })
-
